0b9ce376fa878935b6c6b5a229a4b0bbc57c902f
Elliot Saint-andré <elliotstdr@macbook-air-de-elliot-2.home>
2023-06-08 23:53:32 +0200

    [ADD] Card detail

--- src/Components/RecipeContainer/RecipeCard/CardDetail/CardDetail.js
+++ src/Components/RecipeContainer/RecipeCard/CardDetail/CardDetail.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./CardDetail.scss";
 import { useFetchGet } from "../../../../Services/api";
 import Loader from "../../../../Utils/Loader/loader";
 import PropTypes from "prop-types";
+import default2 from "../../../../assets/default2.jpg";
+import { GiKnifeFork } from "react-icons/gi";
+import { BsPeople } from "react-icons/bs";
+import { BiTimer } from "react-icons/bi";
+import { BiAward } from "react-icons/bi";
+import { Divider } from "primereact/divider";
 
 const CardDetail = (props) => {
   const recipeDetail = useFetchGet(`/recipes/${props.id}`);
@@ -10,39 +17,71 @@ const CardDetail = (props) => {
     <div className="cardDetail_container">
       {recipeDetail.loaded ? (
         <>
-          <div className="cardDetail_container_author">
-            Créée par {recipeDetail.data.postedByUser.name}
+          <div className="cardDetail_container_image">
+            <img
+              src={
+                recipeDetail.data.imageUrl
+                  ? process.env.REACT_APP_BASE_URL_API +
+                    recipeDetail.data.imageUrl
+                  : default2
+              }
+              alt="Fond news"
+            />
           </div>
-          <div className="cardDetail_container_title">
+          <h2 className="cardDetail_container_title">
             {recipeDetail.data.title}
+          </h2>
+          <div className="cardDetail_container_author">
+            {recipeDetail.data.postedByUser.imageUrl && (
+              <img
+                src={
+                  process.env.REACT_APP_BASE_URL_API +
+                  recipeDetail.data.postedByUser?.imageUrl
+                }
+                alt="ma pp"
+                className="creatorPP"
+              ></img>
+            )}
+            <span>Créé par {recipeDetail.data.postedByUser?.name}</span>
           </div>
-          <div className="cardDetail_container_time">
-            Temps : {recipeDetail.data.time}
-          </div>
-          <div className="cardDetail_container_number">
-            Pour {recipeDetail.data.number} personnes
-          </div>
-          <div className="cardDetail_container_infos">
+          <div className="cardDetail_container_group">
+            <div className="cardDetail_container_time">
+              <BiTimer></BiTimer> {recipeDetail.data.time}
+            </div>
+            <div className="cardDetail_container_number">
+              <BsPeople></BsPeople> {recipeDetail.data.number} personnes
+            </div>
             <div className="cardDetail_container_infos_type">
-              Type : {recipeDetail.data.type.label}
+              <GiKnifeFork></GiKnifeFork> {recipeDetail.data.type.label}
             </div>
             <div className="cardDetail_container_infos_regime">
-              Regime : {recipeDetail.data.regime.label}
+              <BiAward></BiAward> {recipeDetail.data.regime.label}
             </div>
           </div>
+          <ul className="cardDetail_container_ingredients">
+            <h2 className="ingredient_title">Ingrédients :</h2>
+            {recipeDetail.data.ingredients
+              .sort((a, b) => a.type - b.type)
+              .map((ingredient, index) => (
+                <li className="cardDetail_container_ingredient" key={index}>
+                  {ingredient.quantity} {ingredient.unit.label} de{" "}
+                  <strong>{ingredient.label}</strong>
+                </li>
+              ))}
+          </ul>
           {recipeDetail.data.steps
             .sort((a, b) => a.stepIndex - b.stepIndex)
             .map((step, index) => (
-              <div className="cardDetail_container_step" key={index}>
-                {step.description}
+              <div className="cardDetail_container_block">
+                <div className="cardDetail_container_block_index">
+                  {index + 1}
+                </div>
+                <Divider></Divider>
+                <div className="cardDetail_container_block_step" key={index}>
+                  {step.description}
+                </div>
               </div>
             ))}
-          {recipeDetail.data.ingredients.map((ingredient, index) => (
-            <div className="cardDetail_container_ingredient" key={index}>
-              {ingredient.quantity}
-              {ingredient.unit.label} de {ingredient.label}
-            </div>
-          ))}
         </>
       ) : (
         <Loader></Loader>
